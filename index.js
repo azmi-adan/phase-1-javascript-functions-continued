bd8c62685b897896dff7311a9f8f6d3e3d41b1df
azmi adan <azmi.adan2032@gmail.com>
2024-06-27 06:37:16 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+
+// Function to plan activities for Saturday
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Function to plan work for Monday
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+// Function to create a wrapper function for adjectives
+function wrapAdjective(flair = '*') {
+    return function(adjective = 'special') {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+// Example usage:
+console.log(saturdayFun()); // Output: "This Saturday, I want to roller-skate!"
+console.log(saturdayFun('hike')); // Output: "This Saturday, I want to hike!"
+
+console.log(mondayWork()); // Output: "This Monday, I will go to the office."
+console.log(mondayWork('work from home')); // Output: "This Monday, I will work from home."
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer")); // Output: "You are !!!a dedicated programmer!!!"
+console.log(wrapAdjective("%")("a diligent learner")); // Output: "You are %a diligent learner%!"
